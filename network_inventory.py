b6e4db8a99108ab0a77a32fdb365b5887be92a02
Hank Preston <hank.preston@gmail.com>
2021-06-01 13:03:43 -0400

    Loading testbed and connecting to devices

--- network_inventory.py
+++ network_inventory.py
@@ -8,15 +8,25 @@ Goal:
 
 """
 
+from pyats.topology.loader import load
 
 # Script entry point
 if __name__ == "__main__": 
+    import argparse
+
     print("Creating a network inventory script.")
 
     # Load pyATS testbed into script 
+    # Use argparse to determine the testbed file : https://docs.python.org/3/library/argparse.html
+    parser = argparse.ArgumentParser(description='Generate network inventory report from testbed')
+    parser.add_argument('testbed', type=str, help='pyATS Testbed File')
+    args = parser.parse_args()
 
+    # Create pyATS testbed object
+    testbed = load(args.testbed)
 
     # Connect to network devices 
+    testbed.connect()
 
 
     # Run commands to gather output from devices 
